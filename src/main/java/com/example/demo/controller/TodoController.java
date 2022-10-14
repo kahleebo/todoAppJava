e60158c291fbb36c14c9de5e2e4a6b99bd81d50c
Bentialy Julien <bentialy.julien@projectsynergyga.com>
2022-10-14 13:58:12 -0400

    Initial commit

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.model.TodoItem;
 import com.example.demo.repository.TodoRepository;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.aspectj.apache.bcel.Repository;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +34,36 @@ class TodoController {
 
 // Add a PutMapping to update an item by posting the updated object to its REST endpoint
 
+    // build update -ToDoItem- REST API
+    @PutMapping("/todo/{id}")
+    ResponseEntity<TodoItem> updateToDoItemById(@PathVariable long id, @RequestBody TodoItem item) {
+        //get the ToDoItem schema from the repo
+        TodoItem updateItem = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Item does not exist with id: " + id));
+
+        //update each field of the ToDoItem
+        updateItem.setTitle(item.getTitle());
+        updateItem.setDescription(item.getDescription());
+        updateItem.setCompleted(item.getCompleted());
+        updateItem.setPriority(item.getPriority());
+
+        repository.save(updateItem);
+
+        return ResponseEntity.ok(updateItem);
+    }
+    //Delete a ToDoItem by id
+    @DeleteMapping("/todo/{id}")
+    ResponseEntity<TodoItem> DeleteToDoItemById(@PathVariable long id) {
+        //get the ToDoItem from the repo
+        TodoItem updateItem = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Item does not exist with id: " + id));
+
+        if (updateItem.getId().equals(id) ) {
+            repository.deleteById(id);
+            return new ResponseEntity<>(updateItem, HttpStatus.FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
     @PostMapping("/todo")
     ResponseEntity<?> addNewTodo(@RequestBody TodoItem item){
         return new ResponseEntity<>(repository.save(item), HttpStatus.CREATED);
